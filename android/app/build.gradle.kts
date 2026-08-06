1f8da541c13dfc46de54b4034e65801e8c281794
Evgeniy Krasnyanskiy <Evgeniy.Krasnyanskiy@gmail.com>
2026-08-07 02:23:49 +0300

    fix: reduce minWidth to allow 3x1 and 2x1 Android widget resizing

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.xdripwidget.android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.5.2"
+        versionCode = 2
+        versionName = "1.5.3"
     }
 
     buildTypes {
